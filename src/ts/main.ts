e6eed65213adfe29884f3c47e9511576b7438f2e
Holden Greene <hgreene@cmnhospitals.org>
2019-11-10 16:27:57 -0700

    Started prototyping out a toggle

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,3 +2,15 @@
 if (process.env.NODE_ENV === 'development' && (module as any).hot) {
     (module as any).hot.accept();
 }
+
+const button = document.querySelector('.theme-toggle') as HTMLButtonElement;
+const root = document.querySelector(':root') as HTMLElement;
+
+button.addEventListener('click', () => {
+    root.classList.add('light-theme');
+    localStorage.setItem('theme', 'light-theme');
+});
+
+const theme = localStorage.getItem('theme');
+
+root.classList.add(theme);
