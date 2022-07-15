fbb9d332db7e8bf57c3f24d2099795a5817d5462
Holden Greene <holdengreene26@gmail.com>
2022-07-15 15:10:51 -0600

    Moved to Vite and pnpm

--- .postcssrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    map: false,
-    plugins: {
-        autoprefixer: {},
-        cssnano: {}
-    }
-};
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,10 +1,5 @@
 import { themeToggle } from './modules/themeToggle';
 
-// Parcel hot module reload. Removed during build.
-if (process.env.NODE_ENV === 'development' && (module as any).hot) {
-    (module as any).hot.accept();
-}
-
 // Only call this if the browser supports custom properties
 if (window.CSS && CSS.supports('color', 'var(--font-color')) {
     themeToggle();
