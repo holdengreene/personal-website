0a6c75aab08d841bcff405dc181eba3207f646ee
Holden Greene <holdengreene26@gmail.com>
2022-07-16 14:44:45 -0600

    Started move to svelte

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,5 @@
+<slot />
+
+<style lang="scss" global>
+	@use '../scss/main.scss';
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,38 @@
+<svelte:head>
+	<title>Holden Greene | Full Stack Developer</title>
+</svelte:head>
+
+<div class="colored-background">
+	<div class="main-grid">
+		<div class="theme-toggle">
+			<input
+				type="checkbox"
+				id="theme-toggle"
+				class="theme-toggle__checkbox"
+				aria-label="Turn dark theme on and off"
+			/>
+			<label class="theme-toggle__label" for="theme-toggle">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="moon-svg">
+					<path
+						class="moon-svg__path"
+						d="M13.719 1.8A8.759 8.759 0 111.8 13.719c3.335 1.867 7.633 1.387 10.469-1.449s3.318-7.134 1.45-10.47z"
+					/>
+				</svg>
+			</label>
+		</div>
+
+		<main class="main-grid__text">
+			<div class="main-grid__titles">
+				<h1>Holden Greene</h1>
+				<h2>Full Stack Developer in SLC, Utah</h2>
+			</div>
+
+			<div class="main-grid__link-section">
+				<a
+					class="main-grid__link main-grid__link--github"
+					href="https://github.com/holdengreene/">Github</a
+				>
+			</div>
+		</main>
+	</div>
+</div>
